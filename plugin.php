9804820f8a207b4da9326967924b394dd1b886a4
River Fisher <river.bass.fisher@gmail.com>
2022-05-29 21:13:51 -0400

    Updating plugin info

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: guten-form — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: guten-form — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Guten Form
+ * Plugin URI: https://github.com/riverBanjo/guten-form
+ * Description: A WordPress Gutenberg Form Builder
+ * Author: River Fisher
+ * Author URI: https://github.com/riverBanjo
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
